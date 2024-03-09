c84beb3681a8bc936b0f226818ccebfe030fb1d8
Job Talle <jobtalle@hotmail.com>
2024-03-09 12:39:41 +0100

    Refactored

--- build.py
+++ build.py
@@ -16,9 +16,7 @@ months = [
     "December"]
 regex_title = re.compile("(\d*)_0?(\d*)")
 template = open("template/template.html", "r").read()
-
-def make_file(post, index):
-    return "index.html" if index == 0 else post + ".html"
+template_index = open("template/template_index.html", "r").read()
 
 def compress_html(string):
     return re.sub("|".join(map(re.escape, ["    ", "\n"])), "", string)
@@ -33,11 +31,11 @@ def print_posts():
         date = regex_title.search(post)
         post_dates.append(months[int(date[2]) - 1] + " " + date[1])
         post_links.append("<li><a href=\"%s\">%s</a></li>" % (
-            make_file(post, index),
+            post + ".html",
             post_dates[index]))
 
     for index, post in enumerate(posts):
-        with open(make_file(post, index), "w") as file:
+        with open(post + ".html", "w") as file:
             post_index = ""
 
             for link_index, link in enumerate(post_links):
@@ -49,10 +47,10 @@ def print_posts():
                 "$date$": post_dates[index],
                 "$post$": open("posts/" + post + "/content.html").read().replace("src=\"", "src=\"posts\\%s\\" % post),
                 "$previous$": "<span class=\"previous\"><a href=\"%s\">%s</a></span>" % (
-                    make_file(posts[index + 1], index + 1),
+                    posts[index + 1] + ".html",
                     "<< previous") if index < len(posts) - 1 else "",
                 "$next$": "<span class=\"next\"><a href=\"%s\">%s</a></span>" % (
-                    make_file(posts[index - 1], index - 1),
+                    posts[index - 1] + ".html",
                     "next >>") if index > 0 else "",
             }
             pattern = re.compile("|".join(map(re.escape, table.keys())))
@@ -62,6 +60,10 @@ def print_posts():
 
             file.write(compress_html(pattern.sub(replace, template)))
             file.close()
+
+    with open("index.html", "w") as file:
+        file.write(compress_html(template_index.replace("$index$", posts[0] + ".html")))
+        file.close()
 
 def clear_pages():
     for file in os.listdir("."):
